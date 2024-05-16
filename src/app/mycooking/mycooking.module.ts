e15e566e93a0fdf4aff679e1843c14b5757bfb2a
Liza Akter <lizaa6154@gmail.com>
2024-05-16 08:58:02 +0100

    portfolio mine

---
+++ src/app/mycooking/mycooking.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MycookingRoutingModule } from './mycooking-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MycookingRoutingModule
+  ]
+})
+export class MycookingModule { }
